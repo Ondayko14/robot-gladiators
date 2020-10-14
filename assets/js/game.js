8ddf8714affe19276177b7f2ef458e3279773ebf
Brandon O <b.ondayko@gmail.com>
2020-10-14 12:53:45 -0500

    Fixed empty name bug by adding getPlayerName function and replacing the property in the playerInfo OBJ

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,8 +69,16 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your Robots Name?")
+    }
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
